85f6d6c83986c9a6e35eb2c70a8ed32055389aa6
Max <maxsfs11@gmail.com>
2026-03-30 22:23:30 -0300

    corrige conflito no js

--- angelicabrinquedos.js
+++ angelicabrinquedos.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*  Created by: Truzz Blogg  */
 /*  Youtube Link: https://youtu.be/l7W5pHSr1w0  */
 
@@ -6,21 +5,7 @@ let elem_preloader = document.getElementById("preloader");
 let elem_loader = document.getElementById("loader");
 console.log("Testing... Ok");
 
-
-setTimeout(function() {
+setTimeout(function () {
   elem_preloader.classList.remove("preloader");
   elem_loader.classList.remove("loader");
-=======
-/*  Created by: Truzz Blogg  */
-/*  Youtube Link: https://youtu.be/l7W5pHSr1w0  */
-
-let elem_preloader = document.getElementById("preloader");
-let elem_loader = document.getElementById("loader");
-console.log("Testing... Ok");
-
-
-setTimeout(function() {
-  elem_preloader.classList.remove("preloader");
-  elem_loader.classList.remove("loader");
->>>>>>> 7b516e099f8c962bc85b0983bc07b0efc741523c
-  }, 1280);
+}, 1280);
